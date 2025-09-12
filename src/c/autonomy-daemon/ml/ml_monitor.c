3e0cd6dfce96b80e50aa738125a3125a7814db1b
markus-lassfolk <markus.lassfolk@gmail.com>
2025-09-12 20:15:24 +0200

    Remove logx.h include from ml_monitor.c after revert

--- src/c/autonomy-daemon/ml/ml_monitor.c
+++ src/c/autonomy-daemon/ml/ml_monitor.c
@@ -1,6 +1,5 @@
 #include "ml_monitor.h"
 #include "ml_monitor_analytics.h"
-#include "../utils/logx.h"
 #include "../utils/uci_manager.h"
 #include "../core/types.h"
 #include "../starlink/starlink_snow_detection.h"
